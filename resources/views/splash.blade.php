6fd42c21a3023cf61221b220cec9170c4ccdcc2b
gperez13 <giovanni_perez4@yahoo.com>
2020-01-29 17:56:22 -0600

    First commit

---
+++ resources/views/splash.blade.php
@@ -0,0 +1,33 @@
+@extends ('layout')
+
+@section ('content')
+
+<div class="flex-center position-ref full-height">
+
+
+            <div class="content">
+                <div class="title m-b-md">
+
+                   Giovanni's Portfolio
+
+        
+                </div>
+
+                <h3>
+                    Good things are underway
+                </h3>
+
+                <div style="background-color: #252525; padding-top: 1rem;">
+                    I am passionate about building web applications for clients in fast-growing, human-centered industries, 
+                    including FinTech, Higher Education, Healthcare, and Government. I specialize in Outsystems, Swift, and 
+                    PHP development. While I have modified my PHP stack to include Laravel, Vue.js, MySQL, & Forge, I remain 
+                    language and framework agnostic and am comfortable adapting to project requirements. I take pride in 
+                    working to cultivate a working environment conducive to trust, compassion, and joy by actively sharing 
+                    my vulnerabilities. 
+                </div>
+
+
+            </div>
+        </div>
+
+@endsection
